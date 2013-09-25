f2d0206d91d670d3a903e8f63fae655385c5f15d
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-09-25 15:50:38 +0200

    Show how to write NeXus files

--- simulated-detector.py
+++ simulated-detector.py
@@ -12,6 +12,14 @@ from concert.processes.scan import Scanner
 camera = Camera(background=np.random.random((640, 480)))
 
 
+def get_exposure_scanner(min_exposure, max_exposure):
+    def get_mean_frame_value():
+        return np.mean(camera.grab())
+
+    return Scanner(camera['exposure-time'], get_mean_frame_value,
+                   min_exposure, max_exposure)
+
+
 def plot_exposure_scan(min_exposure=1*q.ms, max_exposure=500*q.ms):
     """
     Plot the mean value of the detector image for exposure times between
@@ -19,18 +27,29 @@ def plot_exposure_scan(min_exposure=1*q.ms, max_exposure=500*q.ms):
 
     Returns: a tuple with exposure times and corresponding mean values.
     """
-
-    def get_mean_frame_value():
-        return np.mean(camera.grab())
-
-    scanner = Scanner(camera['exposure-time'], get_mean_frame_value,
-                      min_exposure, max_exposure)
-
+    scanner = get_exposure_scanner(min_exposure, max_exposure)
     camera.start_recording()
     x, y = scanner.run().result()
     camera.stop_recording()
     plt.plot(x, y)
     return x, y
+
+
+def save_exposure_scan(filename, min_exposure=1*q.ms, max_exposure=500*q.ms):
+    """
+    Run an exposure scan and save the result as a NeXus compliant file. This
+    requires that libnexus and NexPy are installed.
+    """
+    try:
+        from concert.ext.nexus import get_scan_result
+
+        scanner = get_exposure_scanner(min_exposure, max_exposure)
+        camera.start_recording()
+        data = get_scan_result(scanner).result()
+        camera.stop_recording()
+        data.save(filename)
+    except ImportError:
+        print("You have to install NexPy first")
 
 
 def show_camera_frame(exposure_time=5*q.ms):
