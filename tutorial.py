4d9c2257bedf1e0034eac0c70456c32324284e83
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-11-24 12:19:12 +0100

    Add tutorial session that matches the docs

---
+++ tutorial.py
@@ -0,0 +1,15 @@
+"""Demonstrating Concert capabilities"""
+import concert
+concert.require("0.6")
+
+import numpy as np
+from concert.quantities import q
+from concert.session.utils import ddoc, dstate, pdoc, code_of
+from concert.devices.cameras.dummy import Camera
+from concert.devices.motors.dummy import Motor
+from concert.devices.storagerings.dummy import StorageRing
+
+
+ring = StorageRing()
+motor = Motor()
+camera = Camera()
