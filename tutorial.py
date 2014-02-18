ffc774ba7ce8f31ce746f0181c03c943e8617625
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-02-18 10:42:25 +0100

    Fix tutorial for 0.7

--- tutorial.py
+++ tutorial.py
@@ -6,10 +6,10 @@ import numpy as np
 from concert.quantities import q
 from concert.session.utils import ddoc, dstate, pdoc, code_of
 from concert.devices.cameras.dummy import Camera
-from concert.devices.motors.dummy import Motor
+from concert.devices.motors.dummy import LinearMotor
 from concert.devices.storagerings.dummy import StorageRing
 
 
 ring = StorageRing()
-motor = Motor()
+motor = LinearMotor()
 camera = Camera()
