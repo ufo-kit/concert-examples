45c35c80576e756e785616b6e84687af38a5bc8c
Tomas Farago <sensej007@email.cz>
2015-02-09 12:00:14 +0100

    Update simulated-detector for 0.10

--- simulated-detector.py
+++ simulated-detector.py
@@ -7,39 +7,37 @@ import numpy as np
 from concert.quantities import q
 from concert.session.utils import pdoc
 from concert.devices.cameras.dummy import Camera
-from concert.processes import scan
+from concert.processes.common import scan
 from concert.async import resolve
 from concert.coroutines.base import broadcast, inject
 from concert.coroutines.sinks import Accumulate
 from concert.ext.viewers import PyplotViewer, PyplotImageViewer
-from concert.helpers import Range
+from concert.helpers import Region
 
 
 # Create a camera with noisy background
 camera = Camera(background=np.random.random((640, 480)))
 
 
-def get_exposure_result(min_exposure, max_exposure):
+def get_exposure_result(region):
     def get_mean_frame_value():
         return np.mean(camera.grab())
 
-    prange = Range(camera['exposure_time'], min_exposure, max_exposure)
-
-    return scan(get_mean_frame_value, prange)
+    return scan(get_mean_frame_value, region)
 
 
-def plot_exposure_scan(min_exposure=1*q.ms, max_exposure=500*q.ms):
+def plot_exposure_scan(min_exposure=1*q.ms, max_exposure=500*q.ms, num_points=10):
     """
     Plot the mean value of the detector image for exposure times between
-    *min_exposure* and *max_exposure*.
+    *min_exposure* and *max_exposure*, use *num_points* data points.
 
     Returns: a tuple with exposure times and corresponding mean values.
     """
     accum = Accumulate()
+    region = Region(camera['exposure_time'], np.linspace(min_exposure, max_exposure, num_points))
 
     with camera.recording():
-        inject(resolve(get_exposure_result(min_exposure, max_exposure)),
-               broadcast(PyplotViewer(style='-o')(), accum()))
+        inject(resolve(get_exposure_result(region)), broadcast(PyplotViewer(style='-o')(), accum()))
 
     return zip(*accum.items)
 
