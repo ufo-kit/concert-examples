42027a19f8423ae595dbab82ae452e9152a2e565
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2024-01-26 17:46:50 +0100

    concert-examples: Adapt experiment example

--- experiment.py
+++ experiment.py
@@ -1,50 +1,53 @@
 """---\nSimulate a data acquisition.
 
+In this simple example we demonstrate the functionality of a bare-metal
+`Experiment` type. `Experiment` deals with a collection of `Acquisition` and
+calls their `acquire` method. `Acquisition` can be local or remote. In this
+example we are working with local `Acquisition` which is basically a coroutine
+function that describes, how the data to be acquired is produced.
+
+Subsequently, we attach a `Consumer` addon to each `Acquisition`. In this case
+our `Consumer` is a `PyQtGraphViewer` object to display the dummy data.
+
+It is essential that we call `start_recording` on the camera device before
+running the experiment. The default state of the `Camera` device is `standby`.
+Calling `start_recording` transitions the state to `recording` and eventually to
+`readout` which makes grabbing frames prossible.
+
 Usage:
-    await main()
+    await camera.start_recording()
+    await exp.run()
 """
 
 import concert
 concert.require("0.31")
 
 import logging
+from typing import Generator
 from functools import partial
+import numpy as np
 from concert.session.utils import ddoc, dstate, pdoc, code_of
 from concert.experiments.base import Acquisition, Experiment
-from concert.experiments.addons import Consumer
+from concert.experiments.addons.local import Consumer
 from concert.devices.cameras.dummy import Camera
-from concert.ext.viewers import PyplotImageViewer
+from concert.ext.viewers import PyQtGraphViewer
 
-LOG = logging.getLogger(__name__)
-
-
+# Instantiate a dummy camera device which simulates dummy poisson noise
+# as frames. It is essential that we call the `start_recording` method
+# on the camera object to ensure that the camera state is `standby` before
+# running the experiment.
 camera = await Camera()
-viewer = await PyplotImageViewer()
+viewer = await PyQtGraphViewer()
 
-
-async def produce(num_frames):
+async def produce(num_frames: int) -> Generator[np.ndarray, None, None]:
+    """Generates dummy data using the camera device"""
     for i in range(num_frames):
         yield await camera.grab()
 
-
-async def consume(producer):
-    i = 1
-
-    async for item in producer:
-        LOG.info("Got frame number {}".format(i))
-        i += 1
-
-
-darks = await Acquisition("darks", partial(produce, 1), consumers=[consume])
-flats = await Acquisition("flats", partial(produce, 2), consumers=[consume])
-radios = await Acquisition("radios", partial(produce, 20), consumers=[consume])
+darks = await Acquisition("darks", np.ndarray, partial(produce, 1))
+flats = await Acquisition("flats", np.ndarray, partial(produce, 2))
+radios = await Acquisition("radios", np.ndarray, partial(produce, 20))
 acquisitions = [darks, flats, radios]
 
-
-async def main():
-    """Run the example and output the experiment data to a dummy walker.
-    Also show the images in a live preview addon.
-    """
-    exper = await Experiment(acquisitions)
-    Consumer(exper.acquisitions, viewer)
-    await exper.run()
+exp = await Experiment(acquisitions)
+await Consumer(viewer, exp.acquisitions)
